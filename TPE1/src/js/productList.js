e04a6bf28d9dce24a06fd0ff2834d5b8a86e4ca7
Matias Gualino <matias.gualino@gmail.com>
2014-05-21 00:59:22 -0300

    Items por pagina, validacion registro

--- TPE1/src/js/productList.js
+++ TPE1/src/js/productList.js
@@ -1,4 +1,5 @@
 var page_size = 4;
+var gridType = false;
 $(function() {
 
 $.when(
@@ -20,25 +21,23 @@ $.when(
     	}
     );
 	
-
+$("#cantPagina").change(function(){
+  			page_size = $(this).val();
+  			loadProducts(gridType);
+  		});
 
 	function loadProducts(grid)
 	{
-		$("#cantPagina").change(function(){
-  			page_size = $(this).val();
-  			loadProducts(grid);
-
-  		});
+		gridType = grid;
 		var html = "<p class='bold big error textCenter center'>No hay productos en la API para la categoria seleccionada</p>";
 		$("#toolbar-list").hide();
 	    $("#pagination-list").hide();
-		$("#products-list").empty();
+		$("#breadcrumbs-two").empty();
 		$("#products-list").empty();
 		var queryParams = parseQueryString();
 		queryParams.each(
 		function(key, value, pos)
 		{
-			$("#breadcrumbs-two").empty();
 			$("#breadcrumbs-two").append("<li><a href='./index.html'>Inicio</a></li>");
 			var id = $.base64('decode', value);
 			var url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsBy";
@@ -47,8 +46,11 @@ $.when(
 				url += "Subc";
 				$.getJSON("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetSubcategoryById&id=" + id + "&callback=?", 
 			        function(result) {
-			        	$("#breadcrumbs-two").append("<li><a href='./product-list.html?categoryId=" + $.base64('encode', result.subcategory.category.id) + "'>" + result.subcategory.category.name + "</a></li>");
-			        	$("#breadcrumbs-two").append("<li><a href='./product-list.html?subCategoryId=" + $.base64('encode', result.subcategory.id) + "'>" + result.subcategory.name + "</a></li>");
+			        	if(result.error === undefined)
+			        	{
+							$("#breadcrumbs-two").append("<li><a href='./product-list.html?categoryId=" + $.base64('encode', result.subcategory.category.id) + "'>" + result.subcategory.category.name + "</a></li>");
+			        		$("#breadcrumbs-two").append("<li><a href='./product-list.html?subCategoryId=" + $.base64('encode', result.subcategory.id) + "'>" + result.subcategory.name + "</a></li>");
+			        	}
 			        }
 			    );
 			}
@@ -57,62 +59,65 @@ $.when(
 				url += "C";
 				$.getJSON("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetCategoryById&id=" + id + "&callback=?", 
 			        function(result) {
-			        	$("#breadcrumbs-two").append("<li><a href='./product-list.html?categoryId=" + $.base64('encode', id) + "'>" + result.category.name + "</a></li>");
+			        	if(result.error === undefined)
+			        	{
+			        		$("#breadcrumbs-two").append("<li><a href='./product-list.html?categoryId=" + $.base64('encode', id) + "'>" + result.category.name + "</a></li>");
+			        	}
 			        }
 			    );
 			}
 
 			url += "ategoryId&id=" + id;
 
-			$.getJSON(url + "&sort_order=desc&callback=?", 
+			$.getJSON(url + "&page_size=" + page_size + "&sort_order=desc&callback=?", 
 		        function(result) {
 		            var products = result.products;
-		            if(products.length > 0) 
+		            if(products != undefined && products.length > 0) 
 	            	{
 	            		html = '';
 	            		$("#toolbar-list").show();
 	            		$("#pagination-list").show();
-	            	}
-		            for(var i = 0; i < products.length; i++)
-		            {
-		            	if(grid)
-		            	{
-		            		if((i%3) == 0)
+			            for(var i = 0; i < products.length; i++)
+			            {
+			            	if(grid)
 			            	{
-			            		if(i > 0) html += "</ul>";
-			            		html += "<ul class='products-grid' >";
-			            	}
-			            	if((i%3) == 2)
-			            	{
-								html += getHTMLProduct(products[i], 1, false, true);
+			            		if((i%3) == 0)
+				            	{
+				            		if(i > 0) html += "</ul>";
+				            		html += "<ul class='products-grid' >";
+				            	}
+				            	if((i%3) == 2)
+				            	{
+									html += getHTMLProduct(products[i], 1, false, true);
+				            	}
+				            	else
+				            	{
+									html += getHTMLProduct(products[i], 1, false, false);
+				            	}
 			            	}
 			            	else
 			            	{
-								html += getHTMLProduct(products[i], 1, false, false);
+								html += "<li class='item '> <a href='product.html?productId=" + $.base64('encode', products[i].id) + "' class='product-image' > " +
+		                		"<img alt='product' src='" + products[i].imageUrl[0] + "'></a>" +
+								"<div class='product-shop'>" + 
+								"<div class='no-fix'>" +
+								"<h2 class='product-name'><a href='product.html?productId=" + $.base64('encode', products[i].id) + "' >" + products[i].name + "</a></h2>" +
+								"<div class='price-box'> <span class='regular-price' >" +
+								"<span class='price'>" + products[i].price + "</span> </span> </div>" +
+								" <div class='ratings'> " +
+									" <div class='rating-box'> " +
+								" <div class='rating' style='width:" + Math.floor(Math.random() * 101) + "%'></div> " +
+								" </div> " + " </div> " +
+								" <div class='btn-set'> " +
+								" <div class='actions'><a class='btn-circle first-bg btn-active' href='./product.html?productId=" + $.base64('encode', products[i].id) + "'> " +
+								"Comprar <i class='icon-shopping-cart'></i> </a> <a class='btn-circle first-bg-hover'> " +
+								" <i class='icon-heart'></i> </a>  " +
+								" </div>" + " </div> " + " <br><hr> " +
+								"<div class='desc std'> Los productos no tienen comentarios en la API. </div> " +
+								"</div> " + " </div> " + "</li>";
 			            	}
-		            	}
-		            	else
-		            	{
-							html += "<li class='item '> <a href='product.html?productId=" + $.base64('encode', products[i].id) + "' class='product-image' > " +
-	                		"<img alt='product' src='" + products[i].imageUrl[0] + "'></a>" +
-							"<div class='product-shop'>" + 
-							"<div class='no-fix'>" +
-							"<h2 class='product-name'><a href='product.html?productId=" + $.base64('encode', products[i].id) + "' >" + products[i].name + "</a></h2>" +
-							"<div class='price-box'> <span class='regular-price' >" +
-							"<span class='price'>" + products[i].price + "</span> </span> </div>" +
-							" <div class='ratings'> " +
-								" <div class='rating-box'> " +
-							" <div class='rating' style='width:" + Math.floor(Math.random() * 101) + "%'></div> " +
-							" </div> " + " </div> " +
-							" <div class='btn-set'> " +
-							" <div class='actions'><a class='btn-circle first-bg btn-active' href='./product.html?productId=" + $.base64('encode', products[i].id) + "'> " +
-							"Comprar <i class='icon-shopping-cart'></i> </a> <a class='btn-circle first-bg-hover'> " +
-							" <i class='icon-heart'></i> </a>  " +
-							" </div>" + " </div> " + " <br><hr> " +
-							"<div class='desc std'> Los productos no tienen comentarios en la API. </div> " +
-							"</div> " + " </div> " + "</li>";
-		            	}
-		            }
+			            }
+			        }
 	            	if(grid)
 	            	{
 		            	$("#products-list").hide();
--- TPE1/src/js/register.js
+++ TPE1/src/js/register.js
@@ -3,4 +3,61 @@ $(function() {
 	$("#email").blur(function(){
 		$("#username").val($("#email").val());
 	});
+$("#btnRegister").click(function(){
+	$("#register-form").submit();
+});
+$("#register-form").validate({
+		rules: {
+			firstname: "required",
+			lastname: "required",
+			username: {
+				required: true,
+				minlength: 2
+			},
+			password: {
+				required: true,
+				minlength: 5
+			},
+			confirm_password: {
+				required: true,
+				minlength: 5,
+				equalTo: "#password"
+			},
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		messages: {
+			firstname: "Please enter your firstname",
+			lastname: "Please enter your lastname",
+			username: {
+				required: "Please enter a username",
+				minlength: "Your username must consist of at least 2 characters"
+			},
+			password: {
+				required: "Please provide a password",
+				minlength: "Your password must be at least 5 characters long"
+			},
+			confirm_password: {
+				required: "Please provide a password",
+				minlength: "Your password must be at least 5 characters long",
+				equalTo: "Please enter the same password as above"
+			},
+			email: "Please enter a valid email address"
+		}
+	});
+
+/*	$("#btnRegister").click(function(){
+		var firstname = $("#firstname").val();
+		var lastname = $("#lastname").val();
+		var email = $("#email").val();
+		var username = $("#username").val();
+		var password = $("#password").val();
+		var confirmPassword = $("#confirm_password").val();
+		var gen = $("#genero").val();
+		var dni = $("#dni").val();
+		var birth = $("#date").val();
+		alert("Te queres registrar.");
+	});*/
 });
