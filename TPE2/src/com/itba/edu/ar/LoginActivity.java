2e4d3c0069abe472e6cc05521f15c503c61d729e
lbadi <leonelbadi@hotmail.com>
2014-06-16 21:04:04 -0300

    Login implementado, guarda el token y el usuario en sharedPreferences

--- TPE2/src/com/itba/edu/ar/LoginActivity.java
+++ TPE2/src/com/itba/edu/ar/LoginActivity.java
@@ -1,8 +1,18 @@
 package com.itba.edu.ar;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import ar.edu.itba.hciapi.api.Api;
+import ar.edu.itba.hciapi.api.ApiCallback;
+import ar.edu.itba.hciapi.model.SignInResult;
+
 
 /**
  * Activity which displays a login screen to the user, offering registration as
@@ -13,10 +23,52 @@ public class LoginActivity extends Activity {
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		//SharedPreferences data = getPreferences(MODE_PRIVATE);
+		//if(data.getString("token", defValue))
 		setContentView(R.layout.activity_login);
  
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		setupButton();
+		
+	}
+	
+	private void setupButton(){
+		Button button = (Button) findViewById(R.id.btnLogin);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				EditText editUsr = (EditText) findViewById(R.id.editUsr);
+				EditText editPass = (EditText) findViewById(R.id.editPass);
+				Api.get().signIn(editUsr.getText().toString(), editPass.getText().toString()
+						, new ApiCallback<SignInResult>() {
+
+					@Override
+					public void call(SignInResult result, Exception exception) {
+						String msg = "";
+        				if (exception != null) {
+        					msg = exception.getMessage();
+        				} else {
+        					SharedPreferences saveData = getPreferences(MODE_PRIVATE);
+        					SharedPreferences.Editor editor = saveData.edit();
+        					editor.putString("token", result.getToken());
+        					editor.putString("user", result.getAccount().getUsername());
+        					editor.commit();
+        					
+        					msg += "token: " + result.getToken();
+        					msg += " username: " + result.getAccount().getUsername();
+        					
+        					Intent intent = new Intent( LoginActivity.this, CategoriesActivity.class);
+            				startActivity(intent);
+        				}
+        			
+        			}
+						
+					
+				});
+				
+			}
+		});
 		
 	}
 
